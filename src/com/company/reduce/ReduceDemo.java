d99dbbdc785055f5fb5f3c34b71ac9ec679b4adf
Michael Steinert <Michael.Steinert.94@gmail.com>
2021-05-21 11:05:49 +0200

    Added some Comments

--- src/com/company/reduce/ReduceDemo.java
+++ src/com/company/reduce/ReduceDemo.java
@@ -7,9 +7,11 @@ public class ReduceDemo {
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(2, 1, 3);
 
+        /* mapToInt() transforms a Stream<Object> to a Stream<int>*/
         int sum = integerList.stream().mapToInt(value -> value).sum();
         System.out.println(sum);
 
+        /* reduce() combines a Stream of Int and produces a aggregated Result */
         Integer reduceSum = integerList.stream().reduce(0, (a, b) -> a + b);
         System.out.println(reduceSum);
 
